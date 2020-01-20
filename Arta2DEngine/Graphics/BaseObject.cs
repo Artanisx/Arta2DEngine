183845b91c55a46d48f21b55aa6074adb5c0d06e
Fabrizio Tobia <fabrizio.tobia@gmail.com>
2020-01-20 15:21:48 +0100

    Added a base abstract class for Game Objects and updated GameObject.cs accordingly.

---
+++ Arta2DEngine/Graphics/BaseObject.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Arta2DEngine.Graphics
+{
+    /// <summary>
+    /// This is the base class for each Game Object. 
+    /// This cannot be instanced, but has to be inherited and its functions needs to be implemented or overriden.
+    /// </summary>
+    public abstract class BaseObject
+    {
+        // Each BaseObject has a texture
+        public Texture2D Texture { get; set; }
+
+        // Each BaseObject has a position
+        public Vector2 Position { get; set; }
+
+        // <summary>
+        /// This method draws the object. It must be called between a spriteBatch.Begin and spriteBatch.End
+        /// It can be overridden if needed.
+        /// </summary>
+        /// <param name="spriteBatch">The spriteBatch from Game().</param
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, Color.White);
+        }
+    }
+}
--- Arta2DEngine/Graphics/GameObject.cs
+++ Arta2DEngine/Graphics/GameObject.cs
@@ -1,24 +1,19 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Arta2DEngine.Graphics;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace Arta2DEngine
+namespace Arta2DEngine.Graphics
 {
     /// <summary>
-    /// This is the main component of each single object inside the game. Currently it's very basic and only contains a Texture2D, a Vector2D for its position and velocity.
+    /// This is the main component of each single object inside the game. Implements the BaseObject class.    
     /// </summary>
-    public class GameObject
+    public class GameObject : BaseObject
     {
-        // Each Gameobject has a texture
-        public Texture2D Texture { get; set; }        
-
-        // A position
-        public Vector2 Position { get; set; }
-
-        // And a velocity
+        // A GameObject also has a velocity
         public Vector2 Velocity { get; set; }
 
         /// <summary>
-        /// This is going to be the box around the objec tot be used for collisions.
+        /// This is going to be the box around the object to be used for collisions.
         /// </summary>        
         public Rectangle BoundingBox
         {
@@ -31,24 +26,6 @@ namespace Arta2DEngine
         }
 
         /// <summary>
-        /// This is going to be a circle around the objec tot be used for collisions.
-        /// </summary>        
-        //public Circle BoundingCircle
-        //{
-        //    get
-        //    {
-        //        // Creates a circle around the object and return it
-        //        Vector2 center;
-        //        center = new Vector2(Position.X + Texture.Width / 2, Position.Y + Texture.Height / 2);
-
-        //        float radius;
-        //        radius = Texture.Width / 2;
-
-        //        return new Circle(center, radius);
-        //    }
-        //}
-
-        /// <summary>
         /// First constructor. We pass a Texture and a Position to create a GameObject
         /// </summary>
         /// <param name="textures">The textures for the game object.</param>
@@ -56,7 +33,7 @@ namespace Arta2DEngine
         public GameObject(Texture2D texture, Vector2 position)
         {
             this.Texture = texture;
-            this.Position = position;
+            this.Position = position;            
         }
 
         /// <summary>
@@ -70,15 +47,6 @@ namespace Arta2DEngine
             this.Texture = texture;
             this.Position = position;
             this.Velocity = velocity;
-        }
-
-        // <summary>
-        /// This simply draws the gameobject. It must be called between a spriteBatch.Begin and spriteBatch.End
-        /// </summary>
-        /// <param name="spriteBatch">The spriteBatch from Game().</param
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            spriteBatch.Draw(Texture, Position, Color.White);
-        }
+        }       
     }
 }
